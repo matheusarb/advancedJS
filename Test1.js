6d6a1599007a4453d9ae6ebc875355fa85de841b
matheusarb <mat.araujoribeiro@gmail.com>
2022-11-19 14:31:53 -0300

    logic problems

--- Test1.js
+++ Test1.js
@@ -1,3 +1,16 @@
-const userName = 'Mahtu';
+// const userName = "Mahtu";
 
-console.log(userName);
+// console.log(userName);
+
+const cam1 = 50;
+const cam2 = 50;
+
+//desconto de 20% na compra das duas
+
+const compraTotal = (item1, item2) => {
+  let desconto = (item1 + item2) * 0.2;
+  let result = item1 + item2 - desconto;
+  return result;
+};
+
+console.log(compraTotal(cam1, cam2));
