51aba424cd9bb16752d5ef4efb1736cc60df8784
Matheus <mat.araujoribeiro@gmail.com>
2022-11-21 11:37:36 -0300

    update

---
+++ 01-Fundamentals-Part-1/starter/assignments1.js
@@ -0,0 +1,5 @@
+let myCountry = "Brazil";
+let myCountryContinent = "South America";
+let myCountryPopulation = `${myCountry} has ${parseInt(210000000)} habitants`;
+
+console.log(myCountryPopulation);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,16 @@
-let js = "amazing";
+// let js = "amazing";
 
-console.log("jonas");
-console.log(23);
+// console.log("jonas");
+// console.log(js);
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "amtehsu");
+
+var123 = 12;
+
+console.log(var123);
