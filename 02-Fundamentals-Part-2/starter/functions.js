741cfd90b95e64d842d5b3024c42a28b4646939d
Matheus <mat.araujoribeiro@gmail.com>
2022-11-28 20:32:15 -0300

    update functions

--- 02-Fundamentals-Part-2/starter/functions.js
+++ 02-Fundamentals-Part-2/starter/functions.js
@@ -1,5 +1,19 @@
 const logger = () => {
-  console.log("my name is matheus");
+  console.log("hello world");
 };
 
 logger();
+logger();
+logger();
+
+//functions are like machines
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+console.log(fruitProcessor(5, 0));
+//parameters are like variables that are specific only to the function
+//they
