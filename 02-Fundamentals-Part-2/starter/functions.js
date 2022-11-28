70124e94ff996ae905d73f310ef7775d1600d31b
Matheus <mat.araujoribeiro@gmail.com>
2022-11-28 20:48:33 -0300

    update functions

--- 02-Fundamentals-Part-2/starter/functions.js
+++ 02-Fundamentals-Part-2/starter/functions.js
@@ -1,22 +1,28 @@
-const logger = () => {
-  console.log("hello world");
-};
+// const logger = () => {
+//   console.log("hello world");
+// };
 
-logger();
-logger();
-logger();
+// const store = logger();
+// // logger();
+// // logger();
+// console.log(store);
 
-//functions are like machines
-//parameters are like variables that are specific only to the function
-//they are also placeholders which holds empty spaces that needs to be filled
+// functions are like machines
+// parameters are like variables that are specific only to the function
+// they are also placeholders which holds empty spaces that needs to be filled
 // when calling the function
 
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `juice with ${apples} apples and ${oranges} oranges`;
-  return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//   const juice = `juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
 
-// remember to store the returned value in a variable!!
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+// // remember to store the returned value in a variable!!
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// FUNCTION DECLARATIONS VS FUNCTION EXPRESSIONS
