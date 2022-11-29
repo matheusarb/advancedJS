a08df771253f09d5096fe17e472078d5d6c45543
Matheus <mat.araujoribeiro@gmail.com>
2022-11-29 11:43:22 -0300

    update functions

---
+++ 02-Fundamentals-Part-2/starter/challenge1.js
@@ -0,0 +1,29 @@
+// TEST DATA 1 AND TEST DATA 2 TOGETHER
+// const avgDolphins = 44 + 23 + 71 / 3;
+// const avgKoalas = 65 + 54 + 49 / 3;
+
+const calcAverage1 = (score1, score2, score3) => score1 + score2 + score3 / 3;
+
+const avgDolphins = Math.round(calcAverage1(44, 23, 71));
+// console.log(avgDolphins);
+const avgKoalas = Math.round(calcAverage1(65, 54, 49));
+// console.log(avgKoalas);
+
+function checkWinner(avgDolphins, avgKoalas) {
+  avgDolphins = calcAverage1(85, 54, 41);
+  avgKoalas = calcAverage1(23, 34, 27);
+
+  if (avgDolphins >= avgKoalas * 2) {
+    console.log(
+      `Dolphins wins! (${Math.round(avgDolphins)} vs. ${Math.round(avgKoalas)})`
+    );
+  } else if (avgKoalas >= avgDolphins * 2) {
+    console.log(
+      `Koalas wins! (${Math.round(avgKoalas)} vs. ${Math.round(avgDolphins)})`
+    );
+  } else {
+    console.log("No winners in the contest");
+  }
+}
+
+console.log(checkWinner(avgDolphins, avgKoalas));
