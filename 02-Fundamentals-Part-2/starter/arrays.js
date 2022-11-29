4b3198acef816e4fdcd61b833f98c672b0e7f3c5
Matheus <mat.araujoribeiro@gmail.com>
2022-11-29 20:19:30 -0300

    update arrays

--- 02-Fundamentals-Part-2/starter/arrays.js
+++ 02-Fundamentals-Part-2/starter/arrays.js
@@ -1,14 +1,28 @@
-const friend1 = "Luiggi";
-const friend2 = "Tata";
-const friend3 = "Xande";
-const friend4 = "Preto";
+// const friend1 = "Luiggi";
+// const friend2 = "Tata";
+// const friend3 = "Xande";
+// const friend4 = "Preto";
 
-const friends = ["Luiggi", "Preto", "Xande", "Tata"];
-console.log(friends);
+// const friends = ["Luiggi", "Preto", "Xande", "Tata"];
+// console.log(friends);
 
-const years = new Array(1991, 1992, 1990, 1988);
-console.log(years[3]);
+// const years22 = new Array(1991, 1992, 1990, 1988);
+// console.log(years22[3]);
 
-console.log(friends[friends.length - 1]);
+// console.log(friends[friends.length - 1]);
+// const firstName = "Matheus";
+// const matheus = [firstName, "Ribeiro", (age = 60 - 30), friends];
+// console.log(matheus);
 
-console.log(friends);
+// function calcAge1(birthYear) {
+//   return `${2022 - birthYear} years old.`;
+// }
+
+// const years = [1992, 1960, 1988, 1991];
+// const age1 = calcAge1(years[0]);
+// const age2 = calcAge1(years[1]);
+// const age3 = calcAge1(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const newYears = [age1, age2, age3];
+// console.log(newYears);
