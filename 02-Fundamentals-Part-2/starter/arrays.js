a1628b6028536c6a7dcb3e36566df81cb2a61994
Matheus <mat.araujoribeiro@gmail.com>
2022-11-29 20:07:05 -0300

    update arrays

---
+++ 02-Fundamentals-Part-2/starter/arrays.js
@@ -0,0 +1,14 @@
+const friend1 = "Luiggi";
+const friend2 = "Tata";
+const friend3 = "Xande";
+const friend4 = "Preto";
+
+const friends = ["Luiggi", "Preto", "Xande", "Tata"];
+console.log(friends);
+
+const years = new Array(1991, 1992, 1990, 1988);
+console.log(years[3]);
+
+console.log(friends[friends.length - 1]);
+
+console.log(friends);
