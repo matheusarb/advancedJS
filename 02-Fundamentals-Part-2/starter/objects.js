c585aa2d17cc84a16536c8eab2e4d36758afd358
Matheus <mat.araujoribeiro@gmail.com>
2022-11-30 17:03:40 -0300

    update fundamentals

--- 02-Fundamentals-Part-2/starter/objects.js
+++ 02-Fundamentals-Part-2/starter/objects.js
@@ -1,3 +1,5 @@
+// this is a object literal
+// key-values pairing. Each key is a property;
 const Matheus = {
   name: ["matheus", "theu"],
   age: 30,
@@ -6,3 +8,10 @@ const Matheus = {
 };
 
 console.log(Matheus);
+console.log(Matheus["profession"]);
+
+// the main difference is that with bracket notation we can use expressions to retrieve
+// data
+
+const nameKey = "name";
+console.log(Matheus[nameKey]);
