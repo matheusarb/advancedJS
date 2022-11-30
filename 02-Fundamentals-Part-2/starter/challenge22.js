99861bbc2f08d4b729e6905d65da548f8e28e580
Matheus <mat.araujoribeiro@gmail.com>
2022-11-30 11:22:05 -0300

    update fundamentals

--- 02-Fundamentals-Part-2/starter/challenge22.js
+++ 02-Fundamentals-Part-2/starter/challenge22.js
@@ -1,24 +1,21 @@
-const bills = [125, 555, 44];
-const calcTip = (bill) => {
-  let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-  bills.push(tip);
+// const bills = [125, 555, 44];
+// const calcTip = (bill) => {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
 
-  return tip;
-};
+// //console.log(calcTip(100));
 
-//console.log(calcTip(100));
+// const tips = [];
+// tips.push(calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]));
+// console.log(tips);
 
-const tips = [];
-tips.push(calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]));
-console.log(tips);
+// const totalValues = new Array(
+//   bills[0] + tips[0],
+//   bills[1] + tips[1],
+//   bills[2] + tips[2]
+// );
 
-const totalValues = new Array(
-  bills[0] + tips[0],
-  bills[1] + tips[1],
-  bills[2] + tips[2]
-);
-
-console.log(totalValues);
+// console.log(totalValues);
 
 //console.log(calcTip(125));
 //console.log(`The bill is ${bill} and the tip is ${tip}.`);
---
+++ 02-Fundamentals-Part-2/starter/objects.js
@@ -0,0 +1,8 @@
+const Matheus = {
+  name: ["matheus", "theu"],
+  age: 30,
+  profession: "programmer",
+  bestFriends: "Luiggi, Xande, Tata, Preto, Nino...",
+};
+
+console.log(Matheus);
