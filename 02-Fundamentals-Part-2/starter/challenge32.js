26ef803a82a2d2dd41bfea47bacc97e6395fa3b4
Matheus <mat.araujoribeiro@gmail.com>
2022-11-30 21:24:15 -0300

    update fundamentals

--- 02-Fundamentals-Part-2/starter/challenge32.js
+++ 02-Fundamentals-Part-2/starter/challenge32.js
@@ -1,1 +1,34 @@
+//let GLOBAL_BMI = mass / height ** 2;
 
+const user1 = {
+  fullname: "Mark",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;
+    return this.BMI;
+  },
+};
+
+const user2 = {
+  fullname: "John",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;
+    return this.BMI;
+  },
+};
+
+console.log(user1.calcBMI());
+console.log(user1.BMI);
+console.log(user2.calcBMI());
+console.log(user2.BMI);
+
+user1.BMI > user2.BMI
+  ? console.log(
+      `${user1.fullname}'s BMI(${user1.BMI}) is higher than ${user2.fullname}'s BMI(${user2.BMI})`
+    )
+  : console.log(
+      `${user2.fullname}s BMI(${user2.BMI}) is higher than ${user1.fullname}'s BMI(${user1.BMI})`
+    );
