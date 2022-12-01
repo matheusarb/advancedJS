0f786d45d5d1203f5a00bf3fa73cded2be9aa219
Matheus <mat.araujoribeiro@gmail.com>
2022-12-01 10:33:33 -0300

    exercises update

--- 02-Fundamentals-Part-2/starter/loops.js
+++ 02-Fundamentals-Part-2/starter/loops.js
@@ -61,3 +61,13 @@ const matheusArray = [
 for (let i = matheusArray.length - 1; i >= 0; i--) {
   console.log(i, matheusArray[i]);
 }
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`--------Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(
+      `Exercise ${exercise}:-----------Lifting weights repetition ${rep}`
+    );
+  }
+}
