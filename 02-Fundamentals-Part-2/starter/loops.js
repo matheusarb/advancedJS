b244adfcf7c4822cf91012a0f2852eff46d99989
Matheus <mat.araujoribeiro@gmail.com>
2022-12-01 10:26:15 -0300

    exercises update

--- 02-Fundamentals-Part-2/starter/loops.js
+++ 02-Fundamentals-Part-2/starter/loops.js
@@ -4,36 +4,60 @@
 //   console.log(`lifting! rep number ${rep} 🏋️‍♂️`);
 // }
 
+// const matheusArray = [
+//   "Matheus",
+//   "Ribeiro",
+//   2022 - 1992,
+//   "programmer",
+//   ["Luiggi", "Tata", "Xande"],
+//   true,
+// ];
+
+// const types = [];
+
+// for (let i = 0; i < matheusArray.length; i++) {
+//   console.log(matheusArray[i], typeof matheusArray);
+
+//   //types[i] = typeof matheusArray[i];
+//   types.push(typeof matheusArray[i]);
+// }
+
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i]);
+// }
+
+// console.log(ages);
+
+// CONTINUE STATEMENT
+// console.log("---STRINGS ONLY---");
+// for (let i = 0; i < matheusArray.length; i++) {
+//   // if the typeof value is different of a string, go to the other index/position to check it.
+//   // only print strings
+//   if (typeof matheusArray[i] !== "string") continue;
+//   console.log(matheusArray[i], typeof matheusArray[i]);
+// }
+
+// console.log("---BREAK WITH NUMBER---");
+// for (let i = 0; i < matheusArray.length; i++) {
+//   // if the typeof value is different of a string, go to the other index/position to check it.
+//   // only print strings
+//   if (typeof matheusArray[i] === "number") break;
+//   console.log(matheusArray[i], typeof matheusArray[i]);
+// }
+
 const matheusArray = [
   "Matheus",
   "Ribeiro",
   2022 - 1992,
   "programmer",
   ["Luiggi", "Tata", "Xande"],
-  true,
 ];
 
-const types = [];
-
-for (let i = 0; i < matheusArray.length; i++) {
-  console.log(matheusArray[i], typeof matheusArray);
-
-  //types[i] = typeof matheusArray[i];
-  types.push(typeof matheusArray[i]);
-}
-
-console.log(types);
-
-const years = [1991, 2007, 1969, 2020];
-const ages = [];
-
-for (let i = 0; i < years.length; i++) {
-  ages.push(2037 - years[i]);
-}
-
-console.log(ages);
-
-// CONTINUE STATEMENT
-for (let i = 0; i < matheusArray.length; i++) {
-  console.log(matheusArray[i], typeof matheusArray[i]);
+for (let i = matheusArray.length - 1; i >= 0; i--) {
+  console.log(i, matheusArray[i]);
 }
