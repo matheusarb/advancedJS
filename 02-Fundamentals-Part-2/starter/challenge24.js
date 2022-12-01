7348fa5e1eb7a29e6e344f7daaf23e3274801157
Matheus <mat.araujoribeiro@gmail.com>
2022-12-01 16:08:24 -0300

    exercises update

--- 02-Fundamentals-Part-2/starter/challenge24.js
+++ 02-Fundamentals-Part-2/starter/challenge24.js
@@ -1,20 +1,42 @@
 const BILLS = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+for (let b = 0; b <= BILLS.length; b++) {
+  //console.log(BILLS[b]);
+}
+
 const tips = [];
 const totals = [];
 
 function calcTip(bill) {
-  let tip = 0;
-  tip = bill > 50 && bill < 300 ? tip * 0.15 : tip * 0.2;
+  let tip = bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
 
-  // loop through bills array and generate tips for each bill inside
-  for (let b = 0; b.length <= BILLS; b++) {
-    tips = tips.push(calcTip(BILLS[b]));
-    for (let t = 0; t.length <= tips; t++) {
-      totals = totals.push([BILLS[b] + tips[t]]);
-    }
+  for (let i = 0; i <= BILLS.length; i++) {
+    tips.push(calcTip(BILLS[i]));
+  }
+  for (let j = 0; j <= tips.length; j++) {
+    totals = BILLS[i] + tips[j];
   }
 
   return totals;
 }
 
-console.log(calcTip(BILLS));
+// function calcAv(arr) {
+//     let sum = 0;
+//     for (let sum; sum <= arr.length; sum++) {
+//         sum += arr[i];
+
+//     }
+// }
+
+console.log(calcTip(100));
+//console.log(calcTip(22));
+
+// loop through bills array and generate tips for each bill inside
+// for (let b = 0; b <= BILLS.length; b++) {
+//     tips = tips.push(calcTip(BILLS[b]));
+//     console.log(tips);
+//     for (let t = 0; t <= tips.length; t++) {
+//       totals = totals.push([BILLS[b] + tips[t]]);
+//     }
+//   }
+
+//  // return totals;
