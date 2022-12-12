6377ce6a55783e688b9510332f37646f78097548
Matheus <mat.araujoribeiro@gmail.com>
2022-12-12 11:16:20 -0300

    update ch51

--- 03-Developer-Skills/final/challenge51.js
+++ 03-Developer-Skills/final/challenge51.js
@@ -1,22 +1,35 @@
-const forecastedMaxTemp = [];
+//MY SOLUTION
+let data1 = [17, 21, 23];
+let data2 = [12, 5, -5, 0, 4];
+const printForecast = arr => {
+  let dayNum = 1;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.length === 3) {
+      console.log(
+        `...${arr[0]}°C in ${dayNum} day, ${arr[1]}°C in ${dayNum + 1} days, ${
+          arr[2]
+        }°C in ${dayNum + 2} days...`
+      );
+    } else if (arr.length === 5) {
+      console.log(
+        `...${arr[0]}°C in ${dayNum} day, ${arr[1]}°C in ${dayNum + 1} days, ${
+          arr[2]
+        }°C in ${dayNum + 2} days, ${arr[3]}°C in ${dayNum + 3} days, ${
+          arr[4]
+        }°C in ${dayNum + 4} days...`
+      );
+    }
+  }
+
+  return arr;
+};
+
+console.log(printForecast(data1));
+console.log(printForecast(data2));
+
+//MY SECOND TRY
 let arr1 = [17, 21, 23];
 let arr2 = [12, 5, -5, 0, 4];
 
-const printForecast = arr => {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-
-  if (arr.length === 3) {
-    console.log(
-      `${arr[0]}°C in 1 day, ${arr[1]}°C in 2 days, ${arr[2]}°C in 3 days`
-    );
-  } else if (arr.length === 5) {
-    console.log(
-      `${arr[0]}°C in 1 day, ${arr[1]}°C in 2 days, ${arr[2]}°C in 3 days, ${arr[3]}°C in 4 days, ${arr[4]}°C in 5 days`
-    );
-  }
-};
-
-console.log(printForecast(arr2));
+//INSTRUCTOR SOLUTION
