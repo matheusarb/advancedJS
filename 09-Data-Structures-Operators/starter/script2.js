a77469a1a84fc4963ead148bd7d3d4d087bff783
Matheus <mat.araujoribeiro@gmail.com>
2023-01-24 22:22:41 -0300

    spread operator

--- 09-Data-Structures-Operators/starter/script2.js
+++ 09-Data-Structures-Operators/starter/script2.js
@@ -1,7 +1,6 @@
-//1. DESTRUCTURING OBJECTS
-
 'use strict';
 
+//1. DESTRUCTURING OBJECTS
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -39,51 +38,49 @@ const restaurant = {
   },
 };
 
-restaurant.orderDelivery({
-  time: '22:30',
-  address: 'Rua Parambu, 295',
-  mainIndex: 2,
-  starterIndex: 2,
-});
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Rua Parambu, 295',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
 
-restaurant.orderDelivery({
-  address: 'Clube Leblon',
-  starterIndex: 3,
-});
+// restaurant.orderDelivery({
+//   address: 'Clube Leblon',
+//   starterIndex: 3,
+// });
 
 //to destructure objects we use curly braces!
 
 //variables with same name as the property name
-const { name, openingHours, categories } = restaurant;
-//console.log(name, openingHours, categories);
+// const { name, openingHours, categories } = restaurant;
+// //console.log(name, openingHours, categories);
 
-//variables with different names from the properties names
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-//console.log(restaurantName, hours, tags);
+// //variables with different names from the properties names
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// //console.log(restaurantName, hours, tags);
 
-//setting default values
-const { menu = [], starterMenu: starters = [] } = restaurant;
-//console.log(menu, starters);
+// //setting default values
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// //console.log(menu, starters);
 
-// Mutating variables
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
+// // Mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
 
-//javascript sees a curly braces opening as a code block. So, to assign values to the
-//destructured variables, we need to wrap everything into parentheses
-({ a, b } = obj);
-//console.log(a, b);
+// //javascript sees a curly braces opening as a code block. So, to assign values to the
+// //destructured variables, we need to wrap everything into parentheses
+// ({ a, b } = obj);
+// //console.log(a, b);
 
-//Nested objects
+// //Nested objects
 
-const {
-  fri: { open, close },
-} = openingHours;
+const fri = [];
 //console.log(open, close);
 
 ///////////////////////////////////////////////////////
@@ -91,7 +88,7 @@ const {
 //2. DESTRUCTURING ARRAYS
 // console.log(restaurant);
 
-// const arr = [2, 3, 4, 5];
+const arriegua = [2, 3, 4, 5];
 
 // const [a, b, c] = arr;
 //when JS sees this array structure on the left side of the equal assignment
@@ -126,5 +123,35 @@ const {
 // const [i, , [j, k]] = nested;
 // console.log(i, j, k);
 
-// const [p = 1, q = 1, r = 1] = [8, 9];
+const [p = 1, q = 1, r = 1] = [8, 9];
 // console.log(p, q, r);
+
+//3. SPREAD OPERATOR - works on all iterables (arrays, maps, strings, sets
+// except objects)
+//its like taking all the elements out of the array and writing them inside the
+//other one
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+const goodArr = [1, 2, ...arr];
+console.log(goodArr);
+
+//whenever we need to use the individual elements of an array, we use the spread operator
+console.log(...goodArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+//create shallow copies
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//merge arrays together. Join 2 arrays
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+//use on strings
+
+const str = 'matheus';
+const letters = [...str];
+console.log(letters);
