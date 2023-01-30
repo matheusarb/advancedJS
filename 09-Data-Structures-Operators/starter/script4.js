3c50358e0794951f72e5dd4a379ce3b2b1324af2
Matheus <mat.araujoribeiro@gmail.com>
2023-01-29 23:42:17 -0300

    add entries()

--- 09-Data-Structures-Operators/starter/script4.js
+++ 09-Data-Structures-Operators/starter/script4.js
@@ -1,3 +1,47 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+};
 //1. LOGICAL ASSIGNMENT OPERATOR
 
 // 'use strict';
@@ -30,4 +74,17 @@
 // // rest2.numGuests &&= 10;
 
 // console.log(rest1);
-// console.log(rest2);
+console.log('end of section ---');
+
+//2. LOOPING ARRAYS WITH for-of LOOP
+// will give access to each current element of the array when looping through.
+// One by one
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+for (let item of menu) console.log(item);
+
+for (const item of menu.entries()) {
+  console.log(item);
+}
