180804e6e7e3bbc253db057c2d9e7441150ea0cd
Matheus <mat.araujoribeiro@gmail.com>
2023-01-31 20:45:14 -0300

    working with strings

--- 09-Data-Structures-Operators/starter/workingwithstrings.js
+++ 09-Data-Structures-Operators/starter/workingwithstrings.js
@@ -14,4 +14,24 @@ console.log(airline.indexOf('r'));
 // lastIndexOf()
 console.log(airline.lastIndexOf('r'));
 
-//
+// .slice() the length of a slice string is always going to be end - 1
+// OBS: remember that strings are primitives, therefore, they are immutable
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(0, airline.lastIndexOf(' ')));
+
+console.log(airline.slice(-2)); //starts from the end
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  const seatCheck = seat.slice(-1);
+  if (seatCheck === 'B' || seatCheck === 'C')
+    console.log('You got middle seat');
+  else console.log('lucky you!');
+};
+
+checkMiddleSeat('09A');
+checkMiddleSeat('09B');
+checkMiddleSeat('09C');
