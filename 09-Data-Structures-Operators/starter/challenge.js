45dc0022ad21f5b58031a38a3423c8da2f00b9c3
Matheus <mat.araujoribeiro@gmail.com>
2023-01-31 19:16:06 -0300

    working with strings

--- 09-Data-Structures-Operators/starter/challenge.js
+++ 09-Data-Structures-Operators/starter/challenge.js
@@ -168,8 +168,13 @@ console.log(events);
 gameEvents.delete(64);
 
 //3.2
-console.log(
-  `An event happened, on average, every ${90 / gameEvents.size} minutes`
-);
+// const time = [...gameEvents.keys()].pop();
+// console.log(
+//   `An event happened, on average, every ${time / gameEvents.size} minutes`
+// );
 
 //3.3
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
---
+++ 09-Data-Structures-Operators/starter/workingwithstrings.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log('B347'[3]);
+console.log(airline.length);
+console.log('B347'.length);
+
+// indexOf()
+console.log(airline.indexOf('r'));
+
+// lastIndexOf()
+console.log(airline.lastIndexOf('r'));
+
+//
